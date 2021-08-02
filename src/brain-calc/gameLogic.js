76253cd4baee3f7d1d54bc0c70ced1179e9f8023
Genelyov Alexandr <ustus777@mail.ru>
2021-08-02 19:28:38 +0300

    9.6 step

--- src/brain-calc/gameLogic.js
+++ src/brain-calc/gameLogic.js
@@ -6,7 +6,7 @@ export const question = () => {
   const items = ['+', '-', '*'];
   const num1 = getRandomInt(50, 1);
   const num2 = getRandomInt(50, 1);
-  const character = items[getRandomInt(4, 1)];
+  const character = items[getRandomInt(3, 0)];
   const expression = `${num1} ${character} ${num2}`;
   console.log(`Question: ${expression}`);
   return [num1, num2, character];
